d6008710ad2a90187b89044969d875092def7c4b
Aravind Adithya <aravindadithya95@gmail.com>
2017-10-15 04:16:48 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,29 +4,22 @@ DROP TABLE transactions;
 DROP TABLE user_stocks;
 
 CREATE TABLE users(name VARCHAR(20), username VARCHAR(20) PRIMARY KEY, password VARCHAR(20), cash DECIMAL(10, 2), dow30_value DECIMAL(10, 2), overseas_value DECIMAL(10, 2));
-CREATE TABLE stocks(stock_name VARCHAR(20), symbol VARCHAR(20) PRIMARY KEY, sept_price DECIMAL(10, 2));
+CREATE TABLE stocks(stock_name VARCHAR(20), symbol VARCHAR(20) PRIMARY KEY, sept_price DECIMAL(10, 2), sept_price_overseas DECIMAL(10, 2));
 CREATE TABLE transactions(type ENUM('Deposit Cash', 'Withdraw Cash', 'Buy', 'Sell'), username VARCHAR(20), symbol VARCHAR(20), time_stamp TIMESTAMP, shares NUMERIC(10), price DECIMAL(10, 2), cash_value DECIMAL(10, 2), foreign_cash_value DECIMAL(10, 2));
 CREATE TABLE user_stocks(username VARCHAR(20), symbol VARCHAR(20), shares NUMERIC(20), cost_basis DECIMAL(10, 2), category ENUM('dow30', 'overseas'));
 
-INSERT INTO users VALUES("a", "a", "a", 1000, 0, 0);
-INSERT INTO stocks VALUES("Apple Inc.", "AAPL", 200);
-INSERT INTO stocks VALUES("Stock 1", "IS1", 300);
+INSERT INTO stocks VALUES ("Apple Inc.", "AAPL", "164.05", "0");
+INSERT INTO stocks VALUES ("Microsoft Corp.", "MSFT", "73.94", "0");
+INSERT INTO stocks VALUES ("The Walt Disney Co.", "DIS", "101.50", "0");
+INSERT INTO stocks VALUES ("IBM", "IBM", "144.08", "0");
+INSERT INTO stocks VALUES ("Nike Inc.", "NKE", "53.36", "0");
+INSERT INTO stocks VALUES ("Pfizer Inc.", "PFE", "33.96", "0");
+
+INSERT INTO stocks VALUES ("Axis Bank Ltd", "AXISBANK.NS", "0", "507.55");
+INSERT INTO stocks VALUES ("Bharti Airtel Ltd.", "BHARTIARTL.NS", "0", "422.55");
+INSERT INTO stocks VALUES ("TCS Ltd.", "TCS.NS", "0", "2456.45");
+INSERT INTO stocks VALUES ("Kotak Mahindra Ltd.", "KOTAKBANK.NS", "0", "992.55");
 
 SELECT * FROM users;
 SELECT * FROM transactions;
 SELECT * FROM user_stocks;
-
-update stocks set stock_name = "Axis Bank Ltd"  where symbol = "IS1";
-update stocks set symbol = "AXISBANK.NS" where stock_name = "Axis Bank Ltd";
-update stocks set sept_price = "164.05" where symbol = "AAPL";
-update stocks set sept_price = "507.55" where symbol = "AXISBANK.NS";
-
-INSERT INTO stocks VALUES ("Microsoft Corp.", "MSFT", "73.94");
-INSERT INTO stocks VALUES ("The Walt Disney Co.", "DIS", "101.50");
-INSERT INTO stocks VALUES ("IBM", "IBM", "144.08");
-INSERT INTO stocks VALUES ("Nike Inc.", "NKE", "53.36");
-INSERT INTO stocks VALUES ("Pfizer Inc.", "PFE", "33.96");
-
-INSERT INTO stocks VALUES ("Bharti Airtel Ltd.", "BHARTIARTL.NS", "422.55");
-INSERT INTO stocks VALUES ("TCS Ltd.", "TCS.NS", "2456.45");
-INSERT INTO stocks VALUES ("Kotak Mahindra Ltd.", "KOTAKBANK.NS", "992.55");
