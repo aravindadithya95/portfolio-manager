e90510250d9de85353a3bffe4994eaa4ed1fb384
Aravind Adithya <aravindadithya95@gmail.com>
2017-10-15 18:06:28 -0400

    Add index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Portfolio Manager</title>
+  </head>
+  <body>
+  <center>
+    <h1>Portfolio Manager</h1>
+    <a href="login.php">
+      <button name="login">Login</button>
+    </a>
+
+    <br><br>
+
+    <a href="signup.php">
+      <button type="button" name="signup">Sign Up</button>
+    </a>
+  </center>
+  </body>
+</html>
