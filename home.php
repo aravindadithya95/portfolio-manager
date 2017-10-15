2e220c5df1455d8f90692e0d047d80d73552705c
Aravind Adithya <aravindadithya95@gmail.com>
2017-10-15 04:18:41 -0400

    Buy/Sell 70-30 and 10% checks

--- home.php
+++ home.php
@@ -12,12 +12,13 @@ if (!isset($_SESSION['username'])) {
 	<title>Portfolio | Home</title>
 </head>
 <body>
+<center>
 	<form action="scripts/logout.php">
 		<button type="submit" name="logout">Logout</button>
 	</form>
 
 	<br>
-	
+
 	<form action="scripts/buy_sell.php" method="post">
 		<h1>Buy</h1>
 		<select name="buy_stock">
@@ -31,10 +32,10 @@ if (!isset($_SESSION['username'])) {
 			<?php if (isset($_SESSION['buy_stock']) && $_SESSION['buy_stock'] == "IS1") echo "selected";
 			?>>IS1</option>
 		</select>
-		<input type="text" name="buy_shares" placeholder="Shares"
+		<input type="number" step="1" min="1" name="buy_shares" placeholder="Shares"
 		<?php if (isset($_SESSION['buy_shares'])) {echo "value=\"" . $_SESSION['buy_shares'] . "\"";}?>
 		/>
-		<input type="text" name="buy_price" placeholder="Price"
+		<input type="number" name="buy_price" placeholder="Price"
 		<?php if (isset($_SESSION['buy_price'])) {echo "value=\"" . $_SESSION['buy_price'] . "\"";}?>
 		/>
 
@@ -51,20 +52,27 @@ if (!isset($_SESSION['username'])) {
 			?>>IS1</option>
 		</select>
 		<button type="submit" name="select">Select</button>
+		<input type="number" step="1" min="1" name="sell_shares" placeholder="Shares"
+		<?php if (isset($_SESSION['sell_shares'])) {echo "value=\"" . $_SESSION['sell_shares'] . "\"";}?>
+		/>
+		<input type="number" name="sell_price" placeholder="Price"
+		<?php if (isset($_SESSION['sell_price'])) {echo "value=\"" . $_SESSION['sell_price'] . "\"";}?>
+		/>
 
-		<br><br>
+		<br>
 
 		<?php
 		if (isset($_SESSION['select'])) {
 			require 'scripts/database.php';
 
 			$username = $_SESSION['username'];
-			$symbol = $_SESSION['buy_stock'];
+			$symbol = $_SESSION['sell_stock'];
 
 			$query = "SELECT * FROM user_stocks WHERE username = '$username' AND symbol = '$symbol'";
 			$result = mysqli_query($conn, $query);
 			?>
 
+			<br>
 			<table>
 				<tr>
 					<th>Select</th>
@@ -72,22 +80,21 @@ if (!isset($_SESSION['username'])) {
 					<th>Shares</th>
 					<th>Cost Basis</th>
 					<th>Category</th>
-					<th>Enter Price</th>
 				</tr>
 				<?php
 				$count = 0;
 				while ($row = mysqli_fetch_assoc($result)) {
 					?>
 					<tr>
-						<td><input type="checkbox" name=<?php echo "cbox" + $count; ?> /></td>
+						<td><input type="radio" name="radio" value=<?php echo "radio" . ++ $count; ?> /></td>
 						<td><?php echo $row['symbol'] ?></td>
 						<td><?php echo $row['shares'] ?></td>
 						<td><?php echo $row['cost_basis'] ?></td>
 						<td><?php echo $row['category'] ?></td>
-						<td><input type="text" name=<?php echo "price" + $count; ?> value=""></td>
 					</tr>
 					<?php
 				}
+				$_SESSION['count'] = $count;
 				?>
 			</table>
 
@@ -109,7 +116,9 @@ if (!isset($_SESSION['username'])) {
 			<option value="withdraw">Withdraw</option>
 		</select>
 		<input type="text" name="amount" placeholder="Amount"/>
+		<br><br>
 		<button type="submit" name="add">Add to Portfolio</button>
 	</form>
+</center>
 </body>
 </html>
