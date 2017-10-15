764e60c9ae8c62547e6e5d8499582960dae78fec
Aravind Adithya <aravindadithya95@gmail.com>
2017-10-15 12:27:49 -0400

    Separate domestic and overseas buy

--- home.php
+++ home.php
@@ -21,22 +21,55 @@ if (!isset($_SESSION['username'])) {
 
 	<form action="scripts/buy_sell.php" method="post">
 		<h1>Buy</h1>
-		<select name="buy_stock">
+
+		<h3>Domestic</h3>
+		<select name="buy_stock_dow30">
 			<option name="aapl"
-			<?php if (isset($_SESSION['buy_stock']) && $_SESSION['buy_stock'] == "AAPL") echo "selected";
+			<?php if (isset($_SESSION['buy_stock_dow30']) && $_SESSION['buy_stock_dow30'] == "AAPL") echo "selected";
 			?>>AAPL</option>
-			<option name="googl"
-			<?php if (isset($_SESSION['buy_stock']) && $_SESSION['buy_stock'] == "GOOGL") echo "selected";
-			?>>GOOGL</option>
-			<option name="is1"
-			<?php if (isset($_SESSION['buy_stock']) && $_SESSION['buy_stock'] == "IS1") echo "selected";
-			?>>IS1</option>
+			<option name="msft"
+			<?php if (isset($_SESSION['buy_stock_dow30']) && $_SESSION['buy_stock_dow30'] == "MSFT") echo "selected";
+			?>>MSFT</option>
+			<option name="dis"
+			<?php if (isset($_SESSION['buy_stock_dow30']) && $_SESSION['buy_stock_dow30'] == "DIS") echo "selected";
+			?>>DIS</option>
+			<option name="ibm"
+			<?php if (isset($_SESSION['buy_stock_dow30']) && $_SESSION['buy_stock_dow30'] == "IBM") echo "selected";
+			?>>IBM</option>
+			<option name="nke"
+			<?php if (isset($_SESSION['buy_stock_dow30']) && $_SESSION['buy_stock_dow30'] == "NKE") echo "selected";
+			?>>NKE</option>
+			<option name="pfe"
+			<?php if (isset($_SESSION['buy_stock_dow30']) && $_SESSION['buy_stock_dow30'] == "PFE") echo "selected";
+			?>>PFE</option>
 		</select>
-		<input type="number" step="1" min="1" name="buy_shares" placeholder="Shares"
-		<?php if (isset($_SESSION['buy_shares'])) {echo "value=\"" . $_SESSION['buy_shares'] . "\"";}?>
+		<input type="number" step="1" min="1" name="buy_shares_dow30" placeholder="Shares"
+		<?php if (isset($_SESSION['buy_shares_dow30'])) {echo "value=\"" . $_SESSION['buy_shares_dow30'] . "\"";}?>
 		/>
-		<input type="number" name="buy_price" placeholder="Price"
-		<?php if (isset($_SESSION['buy_price'])) {echo "value=\"" . $_SESSION['buy_price'] . "\"";}?>
+		<input type="number" name="buy_price_dow30" placeholder="Price"
+		<?php if (isset($_SESSION['buy_price_dow30'])) {echo "value=\"" . $_SESSION['buy_price_dow30'] . "\"";}?>
+		/>
+
+		<h3>Overseas</h3>
+		<select name="buy_stock_overseas">
+			<option name="axisbank.ns"
+			<?php if (isset($_SESSION['buy_stock_overseas']) && $_SESSION['buy_stock_overseas'] == "AXISBANK.NS") echo "selected";
+			?>>AXISBANK.NS</option>
+			<option name="bhartiartl.ns"
+			<?php if (isset($_SESSION['buy_stock_overseas']) && $_SESSION['buy_stock_overseas'] == "BHARTIARTL.NS") echo "selected";
+			?>>BHARTIARTL.NS</option>
+			<option name="tcl.ns"
+			<?php if (isset($_SESSION['buy_stock_overseas']) && $_SESSION['buy_stock_overseas'] == "TCS.NS") echo "selected";
+			?>>TCS.NS</option>
+			<option name="kotakbank.ns"
+			<?php if (isset($_SESSION['buy_stock_overseas']) && $_SESSION['buy_stock_overseas'] == "KOTAKBANK.NS") echo "selected";
+			?>>KOTAKBANK.NS</option>
+		</select>
+		<input type="number" step="1" min="1" name="buy_shares_overseas" placeholder="Shares"
+		<?php if (isset($_SESSION['buy_shares_overseas'])) {echo "value=\"" . $_SESSION['buy_shares_overseas'] . "\"";}?>
+		/>
+		<input type="number" name="buy_price_overseas" placeholder="Price"
+		<?php if (isset($_SESSION['buy_price_overseas'])) {echo "value=\"" . $_SESSION['buy_price_overseas'] . "\"";}?>
 		/>
 
 		<h1>Sell</h1>
@@ -44,12 +77,33 @@ if (!isset($_SESSION['username'])) {
 			<option name="aapl"
 			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "AAPL") echo "selected";
 			?>>AAPL</option>
-			<option name="googl"
-			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "GOOGL") echo "selected";
-			?>>GOOGL</option>
-			<option name="is1"
-			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "IS1") echo "selected";
-			?>>IS1</option>
+			<option name="msft"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "MSFT") echo "selected";
+			?>>MSFT</option>
+			<option name="dis"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock0'] == "DIS") echo "selected";
+			?>>DIS</option>
+			<option name="ibm"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "IBM") echo "selected";
+			?>>IBM</option>
+			<option name="nke"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "NKE") echo "selected";
+			?>>NKE</option>
+			<option name="pfe"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['bsell_stock'] == "PFE") echo "selected";
+			?>>PFE</option>
+			<option name="axisbank.ns"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "AXISBANK.NS") echo "selected";
+			?>>AXISBANK.NS</option>
+			<option name="bhartiartl.ns"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "BHARTIARTL.NS") echo "selected";
+			?>>BHARTIARTL.NS</option>
+			<option name="tcl.ns"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "TCS.NS") echo "selected";
+			?>>TCS.NS</option>
+			<option name="kotakbank.ns"
+			<?php if (isset($_SESSION['sell_stock']) && $_SESSION['sell_stock'] == "KOTAKBANK.NS") echo "selected";
+			?>>KOTAKBANK.NS</option>
 		</select>
 		<button type="submit" name="select">Select</button>
 		<input type="number" step="1" min="1" name="sell_shares" placeholder="Shares"
