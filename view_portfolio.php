562e236fc46fbfe5016183188ef62254b8205a26
Intley <Intley@Rahuls-MacBook-Pro.local>
2017-10-14 20:42:06 -0400

    Verify price error

--- view_portfolio.php
+++ view_portfolio.php
@@ -2,13 +2,15 @@
 session_start();
 require 'scripts/database.php';
 
+/*
 if (!isset($_SESSION['username'])) {
 	header("location: login.php");
 }
+*/
 
-$username = $_SESSION['username'];
+$username = "a";
 
-$query = "SELECT * FROM USER_STOCKS";
+$query = "SELECT * FROM stocks where symbol = 'AAPL'";
 $result = mysqli_query($conn, $query);
 
 ?>
@@ -32,8 +34,43 @@ $result = mysqli_query($conn, $query);
       <th>Gain %</th>
       <th>Expected Return</th>
     </tr>
+		<?php
+		while ($row = mysqli_fetch_assoc($result)) {
+		?>
     <tr>
+			<td>
+			<?php
+			echo row['stock_name'];
+			?>
+		</td>
+		<td>
+			<?php
+			$stockname = row['symbol'];
+			echo row['symbol'];
+			?>
+		</td>
+		<td>
+			<?php
+			$url = 'https://finance.yahoo.com/quote/' . $stockname;
+
+			$data = file_get_contents($url);
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(true);
+			$dom->loadHTML($data);
+			libxml_clear_errors();
+			$dom->saveHTML();
+
+			$xpath = new DOMXPath($dom);
+			$l_price = $xpath->query('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]/text()');
+			$current = $l_price->item(0)->nodeValue;
+
+			echo $current;
+			?>
+			</td>
     </tr>
+		<?php
+		}
+		?>
   </table>
 </body>
 </html>
